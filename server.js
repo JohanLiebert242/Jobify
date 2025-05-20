eb0654a85e0e96c20b201de2c0dc3dd03619368e
Nguyễn Tuấn Lộc ( aka Johan) <nguyenloc24022005@gmail.com>
2025-05-20 21:01:00 +0700

    Fix: Fix bugs helmet not alloing cloudinary url

--- server.js
+++ server.js
@@ -43,15 +43,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     helmet.contentSecurityPolicy({
-        useDefaults: true,
-        directives: {
-            "img-src": [
-                "https://mysitename.com",
-                "https://res.cloudinary.com/",
-            ],
-            upgradeInsecureRequests: [],
-        },
-        reportOnly: false,
+        "default-src": ["'self'"],
+        "base-uri": ["'self'"],
+        "font-src": ["'self'", "https:", "data:"],
+        "frame-ancestors": ["'self'"],
+        "img-src": ["'self'", "data:", "http://res.cloudinary.com"],
+        "script-src": ["'self'"],
+        "script-src-attr": ["'none'"],
+        "style-src": ["'self'", "https:", "'unsafe-inline'"],
     })
 );
 app.use(mongoSanitize());
